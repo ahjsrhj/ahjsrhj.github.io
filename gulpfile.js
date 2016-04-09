a99347d6c2415c6b8969ca632af564c8143ea01a
rhj <aha199277@qq.com>
2016-04-09 12:04:31 +0800

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+	cssmin = require('gulp-minify-css'),
+	jsmin = require('gulp-uglify'),
+	htmlmin = require('gulp-minify-html');
+	
+
+
+gulp.task("css",function() {
+    return gulp.src(["public/**/*.css","!public/**/*.min.css"])
+    .pipe(cssmin({compatibility: "ie8"}))
+    .pipe(gulp.dest("./dst/"));
+});
+
+gulp.task("js",function() {
+    return gulp.src(["public/**/*.js","!public/**/*.min.js"])
+    .pipe(jsmin())
+    .pipe(gulp.dest("./dst/"));
+});
+
+gulp.task("html",function() {
+    return gulp.src("public/**/*.html")
+    .pipe(htmlmin())
+    .pipe(gulp.dest("./dst/"));
+});
+
+gulp.task("default",["css","js","html"],function() {
+    console.log("gulp task finished!");
+});
